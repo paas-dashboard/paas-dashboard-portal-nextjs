0a605b045a946005587d69ac693d659590fe1a56
ZhangJian He <shoothzj@gmail.com>
2023-05-22 23:41:25 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/static',
+  distDir: 'portal',
+  output: 'export',
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
